085c7fc8c0510e23551f95447d1ed5256de7fc8a
Rakesh <rachakondarakesh157@gmail.com>
2025-09-30 16:47:07 +0530

    industry file csv

--- industry_apis.py
+++ industry_apis.py
@@ -1,13 +1,14 @@
 import polars as pl
 from fastapi import FastAPI
+from pydantic import BaseModel
 df = pl.read_csv('Industry Performance Updated Table - Sheet1.csv')
 
 # print(df.head())
 
 app = FastAPI()
 
-@app.get("/aggregate_category")
-def aggregate_category(category: str):
+@app.get("/aggregate_category/{category}")
+def aggregate_category(category: str | None = None):
     try:
         filtered = df.filter(pl.col("category") == category)
 
@@ -27,3 +28,28 @@ def aggregate_category(category: str):
 
     except Exception as e:
         return {"error": str(e)}
+    
+class FilterRequest(BaseModel):
+    region_name: str
+    category:str | None=None
+    statecode:str |None = None
+
+@app.post('/getdata')
+def region_name_api(filters: FilterRequest):
+    try:
+        filtered = df
+        if filters.region_name:
+            filtered = filtered.filter(pl.col('region_name') == filters.region_name)
+
+        if filters.statecode:
+            filtered = filtered.filter(pl.col("statecode") == filters.statecode)
+
+        if filtered.height == 0:
+            return {'data': []}
+        return {
+            "data":filtered.to_dicts()
+        }  
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
